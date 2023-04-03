9ba8ec1121d8e63a72b625552bb92dd8dd61481a
Raiishin <gavin.tanjy11@gmail.com>
2023-04-03 21:16:23 +0800

    Added :: Initial Structure for App and Backend

---
+++ app/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import ServiceCard from "./components/ServiceCard";
+import React from "react";
+
+const App = () => {
+	return (
+		<div className="text-center">
+			<Navbar />
+			Hello
+			<body className="flex flex-col"></body>
+		</div>
+	);
+};
+
+export default App;
