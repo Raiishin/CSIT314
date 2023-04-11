425bb20ffc541b235bbba842085fd3f8795d0247
Raiishin <gavin.tanjy11@gmail.com>
2023-04-11 20:56:11 +0800

    Added :: Home page and Reusable Movie Card

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,16 +1,42 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import ServiceCard from "./components/ServiceCard";
-import React from "react";
+import './App.css';
+import Navbar from './components/Navbar';
+import MovieCard from './components/MovieCard';
+import React, { useEffect, useState } from 'react';
+
+import { getMovies } from './api/index';
 
 const App = () => {
-	return (
-		<div className="text-center">
-			<Navbar />
-			Hello
-			<body className="flex flex-col"></body>
-		</div>
-	);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const retrieveMovies = async () => {
+      if (movies.length === 0) {
+        const moviesResponse = await getMovies();
+        console.log(moviesResponse);
+
+        // Check if there are any errors
+        if (moviesResponse.errorMessage === '') {
+          setMovies(moviesResponse.items);
+        }
+      }
+    };
+
+    retrieveMovies();
+  }, [getMovies, setMovies]);
+
+  return (
+    <div className="text-center">
+      <Navbar />
+      <body className="grid grid-cols-6">
+        {movies.length > 0 &&
+          movies.map(movie => (
+            <div className="">
+              <MovieCard data={movie} />
+            </div>
+          ))}
+      </body>
+    </div>
+  );
 };
 
 export default App;
