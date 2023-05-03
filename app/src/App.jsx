0959e6cc16b81f24de60bef8228d133cfad9b129
JHc <c_junhao123@hotmail.com>
2023-05-03 23:13:53 +0800

    Added signup page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,17 +1,12 @@
 import Navbar from './components/Navbar';
 import React, { useEffect, useState } from 'react';
 
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Services from './components/Services';
 import ContactUs from './components/ContactUs';
-<<<<<<< HEAD
 import Register from './components/Register';
-=======
-import BookSeats from './components/BookSeats';
-import SignUp from './components/SignUp';
 
->>>>>>> 5f99c6c (Added more styles)
 import Login from './components/Login';
 import ResetPassword from './components/ResetPassword';
 import MovieDetails from './components/MovieDetails';
@@ -20,14 +15,6 @@ import MovieDetails from './components/MovieDetails';
 // import TestSeat from './components/test';
 
 import { getMovies } from './api/movies';
-<<<<<<< HEAD
-
-import Staffweb from './components/Staffweb';
-import Adminweb from './components/Adminweb';
-=======
-import MovieDetails from './components/MovieDetails';
-import TestSeat from './components/test';
->>>>>>> 5f99c6c (Added more styles)
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -59,17 +46,10 @@ const App = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/movie/:id" element={<MovieDetails />} />
-<<<<<<< HEAD
         {/* <Route path="/bookseats" element={<BookSeats />} /> */}
         {/* <Route path="/TestSeat" element={<TestSeat />} /> */}
         <Route path="/staffweb" element={<Staffweb />} />
         <Route path="/adminweb" element={<Adminweb />} />
-=======
-        <Route path="/bookseats" element={<BookSeats />} />
-        <Route path="/TestSeat" element={<TestSeat />} />
-        <Route path="/ResetPassword" element={<ResetPassword />} />
-        <Route path="/register" element={<SignUp />} />
->>>>>>> 5f99c6c (Added more styles)
       </Routes>
     </div>
   );
