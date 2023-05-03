1560206aadd12c5e93622105e04976f8f8ea3ec9
JHc <c_junhao123@hotmail.com>
2023-05-03 22:45:35 +0800

    Added more styles

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,7 +5,13 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import Services from './components/Services';
 import ContactUs from './components/ContactUs';
+<<<<<<< HEAD
 import Register from './components/Register';
+=======
+import BookSeats from './components/BookSeats';
+import SignUp from './components/SignUp';
+
+>>>>>>> 5f99c6c (Added more styles)
 import Login from './components/Login';
 import ResetPassword from './components/ResetPassword';
 import MovieDetails from './components/MovieDetails';
@@ -14,9 +20,14 @@ import MovieDetails from './components/MovieDetails';
 // import TestSeat from './components/test';
 
 import { getMovies } from './api/movies';
+<<<<<<< HEAD
 
 import Staffweb from './components/Staffweb';
 import Adminweb from './components/Adminweb';
+=======
+import MovieDetails from './components/MovieDetails';
+import TestSeat from './components/test';
+>>>>>>> 5f99c6c (Added more styles)
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -48,10 +59,17 @@ const App = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/movie/:id" element={<MovieDetails />} />
+<<<<<<< HEAD
         {/* <Route path="/bookseats" element={<BookSeats />} /> */}
         {/* <Route path="/TestSeat" element={<TestSeat />} /> */}
         <Route path="/staffweb" element={<Staffweb />} />
         <Route path="/adminweb" element={<Adminweb />} />
+=======
+        <Route path="/bookseats" element={<BookSeats />} />
+        <Route path="/TestSeat" element={<TestSeat />} />
+        <Route path="/ResetPassword" element={<ResetPassword />} />
+        <Route path="/register" element={<SignUp />} />
+>>>>>>> 5f99c6c (Added more styles)
       </Routes>
     </div>
   );
