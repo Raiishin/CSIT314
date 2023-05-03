633984f08b7a73ae469bc5f4cd2c805503c2e7d2
JHc <c_junhao123@hotmail.com>
2023-05-03 21:26:21 +0800

    Added user entry restrictions and more stylings.

--- app/src/components/Login.jsx
+++ app/src/components/Login.jsx
@@ -33,42 +33,64 @@ const Login = () => {
   // }, [isAuthenticated]);
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <form className="p-8 rounded-lg shadow-lg max-w-sm w-full" onSubmit={handleSubmit}>
+    <div className="flex flex-col items-center justify-center min-h-screen py-6 bg-gray-50">
+      <h1 className="font-mono text-cyan-600 text-3xl font-large">Login</h1>
+      <form className="p-8 rounded-lg shadow-lg max-w-sm w-full group" onSubmit={handleSubmit}>
         <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+          <h2 className="text-lg text-gray-400 font-medium mb-6">
+            Don't have an account?{' '}
+            <a
+              href="/register"
+              className="text-light-blue text-cyan-600 hover:text-cyan-700 underline underline-offset-2">
+              Register Here
+            </a>{' '}
+          </h2>
+          <label
+            className="text-left text-cyan-600 block text-gray-700 font-medium mb-2"
+            htmlFor="email">
             Email
           </label>
           <input
-            className={`border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline `}
+            className={`border rounded w-full hover:bg-cyan-50 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-cyan-500 focus:ring-1 focus:ring-cyan-500 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 peer`}
             type="email"
-            placeholder="Email"
+            placeholder="Enter your email"
             value={email}
             onChange={handleEmailChange}
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+            required
           />
+          <span class="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+            Please enter a valid email address
+          </span>
         </div>
         <div className="mb-6">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
+          <label
+            className="text-left text-cyan-600 block text-gray-700 font-medium mb-2"
+            htmlFor="password">
             Password
           </label>
           <input
-            className={`border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline `}
+            className={`border rounded w-full hover:bg-cyan-50 py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline focus:border-cyan-500 focus:ring-1 focus:ring-cyan-500 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 peer`}
             type="password"
-            placeholder="Password"
+            placeholder="Enter your password"
             value={password}
             onChange={handlePasswordChange}
+            required
           />
-          <a href="/reset-password" className="text-light-blue hover:text-blue">
-            Forgot your password?
-          </a>
+          <span class="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+            Please enter your password
+          </span>
         </div>
         <div className="items-center justify-between">
           <button
-            className="bg-light-blue hover:bg-blue text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline border-2 border-gray-300 group-invalid:pointer-events-none group-invalid:opacity-30"
             type="submit">
             Sign In
           </button>
         </div>
+        <a href="/reset-password" className="text-cyan-600 hover:text-blue">
+          Forgot your password?
+        </a>
       </form>
     </div>
   );
