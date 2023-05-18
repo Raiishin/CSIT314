0f6f04d696e6e850ffcd4409da0b3109904e2c40
raiishin <gavin.tanjy11@gmail.com>
2023-05-18 21:55:01 +0800

    Added :: Jest test for formatDate function

--- backend/src/test/unit/index.test.js
+++ backend/src/test/unit/index.test.js
@@ -1,8 +1,21 @@
-import { saltPassword } from '../../library/index.js';
+import { saltPassword, formatDate } from '../../library/index.js';
 
 test('Salt passsword', async () => {
   const password = 'password';
   const saltedPassword = saltPassword(password);
 
   expect(saltPassword(password)).toEqual(saltedPassword);
+});
+
+test('Format date', async () => {
+  expect(new Date().getTimezoneOffset()).toBe(0);
+
+  const testDate1 = new Date(new Date(1324567890).toUTCString());
+  expect(formatDate(testDate1)).toMatchInlineSnapshot(`"16 Jan"`);
+
+  const testDate2 = new Date(new Date(13245678900).toUTCString());
+  expect(formatDate(testDate2)).toMatchInlineSnapshot(`"03 Jun"`);
+
+  const testDate3 = new Date(new Date(23235678900).toUTCString());
+  expect(formatDate(testDate3)).toMatchInlineSnapshot(`"26 Sep"`);
 });
